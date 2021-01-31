27358f5fb042990644b2a912f7d79b78573cdde1
Enes Sarar <e.sarar41@gmail.com>
2021-01-31 16:15:57 +0300

    Ders

---
+++ ClassMetotDemo/MusteriManeger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManeger
+    {
+        public void Ekle (Musteri Musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi.\n"+"Id:"+" "+Musteri.Id+"\n"+"Adı Soyadı:"+" "+Musteri.Adi+" " +Musteri.Soyadi);
+            Console.WriteLine("---------------------------------");
+        }
+
+        public void Silme (Musteri Musteri)
+        {
+            Console.WriteLine("Müşteri Silindi.\n" + "Id:" + " " + Musteri.Id + "\n" + "Adı Soyadı:" + " " + Musteri.Adi + " " + Musteri.Soyadi);
+            Console.WriteLine("---------------------------------");
+        }
+
+       
+        
+            
+       
+            
+
+}   
+        
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri Musteri1 = new Musteri();
+            Musteri1.Id = 145;
+            Musteri1.Adi = "Enes";
+            Musteri1.Soyadi = "Salman";
+            Musteri1.Yas = 25;
+
+            Musteri Musteri2 = new Musteri();
+            Musteri2.Id = 146;
+            Musteri2.Adi = "Kemal";
+            Musteri2.Soyadi = "Kırık";
+            Musteri2.Yas = 55;
+
+            Musteri Musteri3 = new Musteri();
+            Musteri3.Id = 147;
+            Musteri3.Adi = "Selman";
+            Musteri3.Soyadi = "kadayıfçı";
+            Musteri3.Yas = 45;
+
+            Musteri[] Musteriler = new Musteri[] { Musteri1, Musteri2, Musteri3 };
+            Console.WriteLine("Müşteri Listesi");
+            foreach (var Musteri in Musteriler)
+            {
+                Console.WriteLine("Id:"+" " +Musteri.Id);
+                Console.WriteLine("Adı:"+" "+Musteri.Adi);
+                Console.WriteLine("Soyadı:"+" "+Musteri.Soyadi);
+                Console.WriteLine("Yaş:"+Musteri.Yas);
+                Console.WriteLine("\n");
+            }          
+            MusteriManeger MusteriManeger = new MusteriManeger();
+            MusteriManeger.Ekle(Musteri1);
+            MusteriManeger.Silme(Musteri2);           
+            
+
+                     
+
+
+
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
